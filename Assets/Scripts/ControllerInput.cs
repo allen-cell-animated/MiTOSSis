50c4288d710e65461200e43b1546a5a22e8b1b27
Blair Lyons <blair208@gmail.com>
2024-10-14 14:36:31 -0700

    built with input systems

--- Assets/Scripts/ControllerInput.cs
+++ Assets/Scripts/ControllerInput.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class ControllerInput : MonoBehaviour 
 {
-    // public RayInteractor rayInteractor; TODO
+    public Transform leftController;
+    public Transform rightController;
 
     static ControllerInput _Instance;
     public static ControllerInput Instance
@@ -21,47 +22,31 @@ public class ControllerInput : MonoBehaviour
 
     public bool GripsDown ()
     {
-        // return (
-        //     OVRInput.Get( OVRInput.Button.PrimaryHandTrigger )
-        //     && OVRInput.Get( OVRInput.Button.SecondaryHandTrigger )
-        // );
-        return false;
+        return Input.GetButton( "XRI_Left_Grip" ) && Input.GetButton( "XRI_Right_Grip" );
     }
 
     public bool LeftTriggerDown ()
     {
-        // return OVRInput.Get( OVRInput.RawButton.LIndexTrigger );
-        return false;
+        return Input.GetButton( "XRI_Left_Trigger" );
     }
 
     public bool RightTriggerDown ()
     {
-        // return OVRInput.Get( OVRInput.RawButton.RIndexTrigger );
-        return false;
+        return Input.GetButton( "XRI_Right_Trigger" );
     }
 
     public Vector3 LeftControllerPosition()
     {
-        // if (!OVRInput.GetControllerPositionValid( OVRInput.Controller.LTouch ))
-        // {
-        //     return Vector3.zero;
-        // }
-        // return OVRInput.GetLocalControllerPosition( OVRInput.Controller.LTouch );
-        return Vector3.zero;
+        return leftController.position;
     }
     
     public Vector3 RightControllerPosition()
     {
-        // if (!OVRInput.GetControllerPositionValid( OVRInput.Controller.RTouch ))
-        // {
-        //     return Vector3.zero;
-        // }
-        // return OVRInput.GetLocalControllerPosition( OVRInput.Controller.RTouch );
-        return Vector3.zero;
+        return rightController.position;
     }
 
     public void ToggleRayInteractors(bool _active)
     {
-        // rayInteractor.gameObject.SetActive( _active );
+        // TODO toggle ray visualization
     }
 }
